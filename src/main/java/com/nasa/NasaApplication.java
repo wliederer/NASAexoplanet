4c0336e786377d0fc309ce3e8e70096f88aa5233
Brittany Tinnin <tinnin.brittany.n@gmail.com>
2020-12-04 11:58:01 -0700

    created load csv method

--- src/main/java/com/nasa/NasaApplication.java
+++ src/main/java/com/nasa/NasaApplication.java
@@ -3,11 +3,36 @@ package com.nasa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class NasaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NasaApplication.class, args);
+		loadCSV();
+	}
+
+	public static void loadCSV() {
+
+		List<List<String>> records  = new ArrayList<>();
+		try{
+
+			BufferedReader br = new BufferedReader(new FileReader("planetsedit.csv"));
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] values = line.split("(\\d+)(,\\s*\\d+)*");
+				records.add(Arrays.asList(values));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(records);
 	}
 
 }
