16d84fd05b43b13aeb7313d095b6326f421e98d0
William Liederer <williamliederer@Williams-MacBook-Pro.local>
2020-12-04 11:56:16 -0600

    first commit

---
+++ src/main/java/com/nasa/NasaApplication.java
@@ -0,0 +1,13 @@
+package com.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApplication.class, args);
+	}
+
+}
