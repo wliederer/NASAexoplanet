f61f69073085097b1717ef2700cfd0c760419f9c
Brittany Tinnin <tinnin.brittany.n@gmail.com>
2020-12-05 19:30:35 -0700

    modified planet class

--- src/main/java/com/nasa/model/Planet.java
+++ src/main/java/com/nasa/model/Planet.java
@@ -1,7 +1,114 @@
 package com.nasa.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by brittanytinnin on 12/4/20 1:14 PM
  */
+@Data
+@NoArgsConstructor
 public class Planet {
+
+    private String planetName;
+    private String hostName;
+    private float defaultParameterSet;
+    private float numberOfStars;
+    private float numberOfPlanets;
+    private String discoveryMethod;
+    private String discoveryYear;
+    private String discoveryFacility;
+    private String solutionType;
+    private int controversialFlag;
+    private String planetaryParameterReference;
+    private float orbitalPeriodDays;
+    private float orbitalPeriodUpper;
+    private float orbitalPeriodLower;
+    private float orbitalPeriodLimit;
+    private float orbitalSemiMajorAxis;
+    private float orbitalSemiMajorAxisUpper;
+    private float orbitalSemiMajorAxisLower;
+    private float orbitalSemiMajorAxisLimit;
+    private float planetRadiusEarth;
+    private float planetRadiusUpperEarth;
+    private float planetRadiusLowerEarth;
+    private float planetRadiusLimitEarth;
+    private float planetRadiusJupiter;
+    private float planetRadiusUpperJupiter;
+    private float planetRadiusLowerJupiter;
+    private float planetRadiusLimitJupiter;
+    private float planetMassEarth;
+    private float planetMassEarthUpper;
+    private float planetMassEarthLower;
+    private float planetMassEarthLimit;
+    private float planetMassJupiter;
+    private float planetMassJupiterUpper;
+    private float planetMassJupiterLower;
+    private float planetMassJupiterLimit;
+    private String planetMassProvenance;
+    private float eccentricity;
+    private float eccentricityUpper;
+    private float eccentricityLower;
+    private float eccentricityLimit;
+    private float insolationFlux;
+    private float insolationFluxUpper;
+    private float insolationFluxLower;
+    private float insolationFluxLimit;
+    private float equilibriumTemperature;
+    private float equilibriumTemperatureUpper;
+    private float equilibriumTemperatureLower;
+    private float equilibriumTemperatureLimit;
+    private float dataShowTransitTimingVariations;
+    private String stellarParameterReference;
+    private float stellarEffectiveTemp;
+    private float stellarEffectiveTempUpper;
+    private float stellarEffectiveTempLower;
+    private float stellarEffectiveTempLimit;
+    private float stellarRadius;
+    private float stellarRadiusUpper;
+    private float stellarRadiusLower;
+    private float stellarRadiusLimit;
+    private float stellarMass;
+    private float stellarMassUpper;
+    private float stellarMassLower;
+    private float stellarMassLimit;
+    private float stellarMetallicity;
+    private float stellarMetallicityUpper;
+    private float stellarMetallicityLower;
+    private float stellarMetallicityLimit;
+    private String stellarMetallicityRatio;
+    private float stellarSurfaceGravity;
+    private float stellarSurfaceGravityUpper;
+    private float stellarSurfaceGravityLower;
+    private float stellarSurfaceGravityLimit;
+    private String systemParameterReference;
+    private String rastr;
+    private String ra;
+    private String decstr;
+    private String dec;
+    private float distance;
+    private float distanceUpper;
+    private float distanceLower;
+    private float vMagnitude;
+    private float vMagnitudeUpper;
+    private float vMagnitudeLower;
+    private float kMagnitude;
+    private float kMagnitudeUpper;
+    private float kMagnitudeLower;
+    private float gaiaMagnitude;
+    private float gaiaMagnitudeUpper;
+    private float gaiaMagnitudeLower;
+    private String dateOfLastUpdate;
+    private String planetaryParameterReferencePubDate;
+    private String releaseDate;
+
+
+
+
+
+
+
+
+
+
 }
