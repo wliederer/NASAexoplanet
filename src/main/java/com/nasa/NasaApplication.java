1db7a3469c06cb7542a21fad7766db4db1bd6883
Brittany Tinnin <tinnin.brittany.n@gmail.com>
2020-12-06 11:30:04 -0700

    add method main

--- src/main/java/com/nasa/NasaApplication.java
+++ src/main/java/com/nasa/NasaApplication.java
@@ -16,6 +16,10 @@ public class NasaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NasaApplication.class, args);
+
+		PlanetMapper planetMapper = new PlanetMapper();
+		List<Planet> planets = planetMapper.convertCsvFileToPlanetObjectList();
+		planetMapper.writePlanetObjectsToJsonFile(planets, "planets.json");
 	}
 
 
